291fff3bc38a8c42897ec8d3f3c129d7a6510b0d
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 11:09:15 +0530

    Added: calculated length using cartesian

--- src/linecomputation/LineComparisionComputation.java
+++ src/linecomputation/LineComparisionComputation.java
@@ -1,10 +1,26 @@
 package linecomputation;
 
+import java.util.Scanner;
+
 public class LineComparisionComputation {
+	public static int calLineLength() {
+		int x1,x2,y1,y2;
+		Scanner length = new Scanner(System.in);
+		System.out.println("Enter the co-ordinates of line");
+		x1 = length.nextInt();
+		x2 = length.nextInt();
+		y1 = length.nextInt();
+		y2 = length.nextInt();
+		int lineLength = (int)Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2) );
+		return lineLength;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to Line\r\n"
+				+ "Comparison Computation\r\n"
+				+ "Program on Master Branch");
+		int lineLength = calLineLength();
+		System.out.println("The length of line is:" + lineLength);
 	}
 
 }
