a99d293a95fcd4a6d38d9efb310c6a0dee19dff0
Sudhi76 <sudhigupta2299@gmail.com>
2022-02-07 11:51:39 +0530

    Added: function for equality of lines

--- src/linecomputation/LineComparisionComputation.java
+++ src/linecomputation/LineComparisionComputation.java
@@ -3,24 +3,38 @@ package linecomputation;
 import java.util.Scanner;
 
 public class LineComparisionComputation {
-	public static int calLineLength() {
-		int x1,x2,y1,y2;
-		Scanner length = new Scanner(System.in);
-		System.out.println("Enter the co-ordinates of line");
-		x1 = length.nextInt();
-		x2 = length.nextInt();
-		y1 = length.nextInt();
-		y2 = length.nextInt();
+	public static int lineLengthCal() {
+		int x1,y1,x2,y2;
+		Scanner line = new Scanner(System.in);
+		System.out.println("Enter the four co-ordinates of line"); 
+		x1 = line.nextInt();
+		y1 = line.nextInt();
+		x2 = line.nextInt();
+		y2 = line.nextInt();
 		int lineLength = (int)Math.sqrt(Math.pow((x1-x2), 2) + Math.pow((y1-y2), 2) );
 		return lineLength;
+		
+	}
+	public static void equalityCheck(int lineLength1 , int lineLength2 ) {
+		if(lineLength1 == lineLength2 ) {
+			System.out.println("Lines are Equal");
+		}
+		else {
+			System.out.println("Lines are not Equal");
+		}
 	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Line\r\n"
 				+ "Comparison Computation\r\n"
 				+ "Program on Master Branch");
-		int lineLength = calLineLength();
-		System.out.println("The length of line is:" + lineLength);
+		int lineLength1 = lineLengthCal();
+		int lineLength2 = lineLengthCal();
+		System.out.println("The length of line 1 is: "+lineLength1);
+		System.out.println("The length of line 2 is: "+lineLength2);
+		equalityCheck(lineLength1,lineLength2);
+		
+
 	}
 
 }
